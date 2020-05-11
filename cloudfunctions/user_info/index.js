d5aec95a90592428478ddae907bfb28c376e00a0
Plane-walker <a990990@163.com>
2020-05-11 19:21:44 +0800

    --story=1001657 --user=planewalker 用户修改信息 https://www.tapd.cn/53043741/s/1003384

---
+++ cloudfunctions/user_info/index.js
@@ -0,0 +1,44 @@
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+
+const db = cloud.database()
+
+const resetable = ['nickName', 'avatar']
+
+exports.main = async (event, context) => {
+  const openId =  cloud.getWXContext().OPENID
+  delete event.userInfo
+  let data = {}
+  let empty = true
+  for(let index = 0; index < resetable.length; index++) {
+    if(resetable[index] in event) {
+      data[resetable[index]] = event[resetable[index]]
+      empty = false
+    }
+  }
+  if(empty) {
+    try {
+      let result = await db.collection('User').doc(openId).get()
+      return result.data
+    }
+    catch(e) {
+      return {_id: null}
+    }
+  }
+  else {
+    try {
+      let result = await db.collection('User').doc(openId).update({
+        data: data
+      })
+      return result.errMsg
+    }
+    catch(e) {
+      return {_id: null}
+    }
+  }
+  
+}
+
