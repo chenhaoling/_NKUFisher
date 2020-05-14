8f7621fa15eb100094bee1691c26c0b9d43d37cd
walter <tjcxs136720@163.com>
2020-05-14 11:16:54 +0800

    finish report fun.

--- cloudfunctions/report/index.js
+++ cloudfunctions/report/index.js
@@ -3,14 +3,56 @@ const cloud = require('wx-server-sdk')
 
 cloud.init()
 
+const db = cloud.database()
+
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
+  const openid = cloud.getWXContext().OPENID
+  
+  if (event.commentId != undefined) {
+    // 投诉评论
+    db.collection('PendingCheck').where({
+      commentId: event.commentId,
+      goodId: event.goodId,
+      reporter: event.reporter
+    }).get().then(res=> {
+      if (res.data.length > 0) {
+        return {detail: false} //已经被该用户投诉过一次
+      }
+      else {
+         db.collection('PendingCheck').add({ 
+          data: event
+        })
+        return {detail: true}
+      }
+    })
 
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+
+  }
+  else  {
+    // 投诉商品
+    db.collection('PendingCheck').where({
+      goodId: event.goodId,
+      reporter: event.reporter
+    }).get().then(res=> {
+      console.log(res.data[0].length)
+      if (res.data.length > 0 ) {
+        for (let i = 0; i < res.data.length; i++) {
+          if (res.data[i].commentId == undefined) {
+            return {detail: false}  //已经被该用户投诉过一次
+          }
+        }
+        db.collection('PendingCheck').add({ 
+          data: event
+        })
+        return {detail: true} 
+      }
+      else {
+         db.collection('PendingCheck').add({ 
+          data: event
+        })
+        return {detail: true}
+      }
+    })
   }
 }
