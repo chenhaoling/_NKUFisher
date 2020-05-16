6f3ffd75707d38db56dc64962207bfd8b80b056e
chentaijie <813947820@qq.com>
2020-05-16 21:42:53 +0800

    --story=1001992 --user=陈泰劼 个人中心页面标签跳转 https://www.tapd.cn/53043741/s/1003465

--- miniprogram/pages/perInfo/perInfo.js
+++ miniprogram/pages/perInfo/perInfo.js
@@ -7,7 +7,20 @@ Page({
   data: {
 
   },
-
+  jump:function(e){
+    wx.navigateTo({
+      url: '../index/index',
+      success: function(res) {
+        console.log("successs");
+      },
+      fail: function(res) {
+        console.log(e);
+      },
+      complete: function(res) {
+    
+      },
+     })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
