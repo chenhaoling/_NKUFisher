5655eca28269d461c221b99e26b2ee96dd28781e
Plane-walker <a990990@163.com>
2020-06-04 15:02:37 +0800

    --story=1003147 --user=planewalker 增加点击量属性 https://www.tapd.cn/53043741/s/1004136

--- cloudfunctions/good_info/index.js
+++ cloudfunctions/good_info/index.js
@@ -7,10 +7,13 @@ const db = cloud.database()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
+  let openId =  cloud.getWXContext().OPENID
   let result = await db.collection('Good').doc(event._id).get()
-  result.data.hot++
-  await db.collection('Good').doc(event._id).update({
-    data: {hot: result.data.hot}
-  })
+  if(openId != result.data.openId) {
+    result.data.hot++
+    await db.collection('Good').doc(event._id).update({
+      data: {hot: result.data.hot}
+    })
+  }
   return result.data
 }
